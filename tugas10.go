831c18edc2220b026dfeb3471f5cb184a3b88c0a
Ready <rhedipratama@gmail.com>
2019-09-07 14:06:26 +0700

    Tugas 10 Golang
    tugas 10 golang (timer)

---
+++ tugas10.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+import "time"
+
+func main()  {
+  fmt.Println("Hai, Nama Saya Rhedi Pratama")
+  time.Sleep(time.Second * 5 )
+  fmt.Println("Hobby Saya Belajar hal baru, dengarkan musik, dan nonton film")
+  time.Sleep(time.Second * 5)
+  fmt.Println("Saya lahir pada tanggal 22 Mei Tahun ****. Terima Kasih")
+}
